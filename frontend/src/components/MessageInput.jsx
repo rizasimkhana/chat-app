c29a89f15cae170564deb05a80e88487d3c35226
rizasimkhana <riza.simkhanas@gmail.com>
2025-04-25 20:21:18 +0530

    add this

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -71,7 +71,7 @@ const MessageInput = () => {
 
       <form onSubmit={handleSendMessage} className="flex items-center gap-2">
         <div className="flex-1 flex gap-2">
-          <input
+        <input
             type="text"
             className="w-full input input-bordered rounded-lg input-sm sm:input-md"
             placeholder="Type a message..."
