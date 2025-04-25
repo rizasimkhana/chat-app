38721f297ae0a6f2cc7f413018ac2ad0b720b619
rizasimkhana <riza.simkhanas@gmail.com>
2025-04-25 18:15:01 +0530

    socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,13 +34,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Static files for production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 // Start the server
 server.listen(PORT, () => {
